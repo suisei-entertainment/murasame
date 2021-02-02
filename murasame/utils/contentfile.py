30e861b8022afa60bd04da552dfa2bdd09935a75
Attila Kovacs <kovee@suiseientertainment.com>
2021-02-02 15:42:51 +0100

    MUR-12: Common functionality split from JsonFile to ContentFile.

--- murasame/utils/contentfile.py
+++ murasame/utils/contentfile.py
@@ -23,31 +23,49 @@ Utility class to help in parsing and handling JSON files.
 
 # Platform Imports
 import os
-import json
 from typing import Callable
 
-# Murasame Imports
-from murasame.exceptions import InvalidInputError
-from murasame.utils.aes import AESCipher
-from murasame.utils.contentfile import ContentFile
-
-class JsonFile(ContentFile):
+class ContentFile:
 
     """
-    Represents a single JSON file on disk. The content of the file can be
+    Represents a single content file on disk. The content of the file can be
     encrypted.
 
     Authors:
         Attila Kovacs
     """
 
+    @property
+    def Path(self) -> str:
+
+        """
+        Provides access to the path of the file.
+
+        Authors:
+            Attila Kovacs
+        """
+
+        return self._path
+
+    @property
+    def Content(self) -> dict:
+
+        """
+        Provides access to the content of the file.
+
+        Authors:
+            Attila Kovacs
+        """
+
+        return self._content
+
     def __init__(self, path: str, cb_retrieve_key: Callable = None) -> None:
 
         """
-        Creates a new JsonFile instance.
+        Creates a new ContentFile instance.
 
         Args:
-            path:               Path to the JSON file on disk.
+            path:               Path to the file on disk.
             cb_retrieve_key:    Optional callback function that will be called
                                 to retrieve the key to decrypt the file.
 
@@ -55,7 +73,80 @@ class JsonFile(ContentFile):
             Attila Kovacs
         """
 
-        super().__init__(path=path, cb_retrieve_key=cb_retrieve_key)
+        self._path = os.path.abspath(path)
+        """
+        Path to the file.
+        """
+
+        self._content = {}
+        """
+        The content of the file.
+        """
+
+        self._cb_retrieve_key = cb_retrieve_key
+
+    def load(self) -> None:
+
+        """
+        Attempts to load the file from disk. If the file doesn't exist, it will
+        create an empty structure in memory.
+
+        Raises:
+            InvalidInputError:      Raised when the file cannot be loaded from
+                                    disk or its content cannot be parsed as a
+                                    valid file.
+
+        Authors:
+            Attila Kovacs
+        """
+
+        if os.path.isfile(self._path):
+
+            if self._cb_retrieve_key is None:
+                self.load_unencrypted()
+            else:
+                self.load_encrypted()
+        else:
+            # Just create an empty structure.
+            self._content = {}
+
+    def save(self, compact: bool = True) -> None:
+
+        """
+        Saves the content of the file to disk.
+
+        Args:
+            compact:    Specifies whether or not the file should be saved in a
+                        compact, less readable format, or in a properly
+                        indented and formatted way. This has no effect on
+                        encrypted files.
+
+        Raises:
+            RuntimeError:       Raised when the file cannot be saved to disk.
+
+        Authors:
+            Attila Kovacs
+        """
+
+        if self._cb_retrieve_key is None:
+            self.save_unencrypted(compact=compact)
+        else:
+            self.save_encrypted(compact=compact)
+
+    def overwrite_content(self, content: dict) -> None:
+
+        """
+        Overwrites the content of the file with the content of a
+        dictionary.
+
+        Args:
+            content:            The new content of the file.
+
+        Authors:
+            Attila Kovacs
+        """
+
+        self._content = content
 
     def save_unencrypted(self, compact: bool = True) -> None:
 
@@ -75,20 +166,11 @@ class JsonFile(ContentFile):
             Attila Kovacs
         """
 
-        # Save the file unencrypted.
-        try:
-            with open(self._path, 'w+') as json_file:
-                if compact:
-                    json.dump(self._content, json_file)
-                else:
-                    json.dump(self._content,
-                              json_file,
-                              indent=4,
-                              separators=(',', ': '))
-        except OSError as exception:
-            raise RuntimeError(
-                'Failed to save the content of JSON file to {}.'.format(
-                    self._path)) from exception
+        del compact
+
+        raise NotImplementedError(
+            f'ContentFile.save_unencrypted() has to be implemented by '
+            f'{self.__class__.__name__}')
 
     def load_unencrypted(self) -> None:
 
@@ -104,20 +186,9 @@ class JsonFile(ContentFile):
             Attila Kovacs
         """
 
-        try:
-            with open(self._path, 'r+') as json_file:
-                # Parse the file and load the content to memory.
-                self._content = json.load(json_file)
-        except OSError as exception:
-            self._content = None
-            raise InvalidInputError(
-                'Failed to read the contents of JSON file {}.'.format(
-                    self._path)) from exception
-        except json.JSONDecodeError as exception:
-            self._content = None
-            raise InvalidInputError(
-                'Failed to parse the content of JSON file {}.'.format(
-                    self._path)) from exception
+        raise NotImplementedError(
+            f'ContentFile.load_unencrypted() has to be implemented by '
+            f'{self.__class__.__name__}')
 
     def save_encrypted(self, compact: bool = True) -> None:
 
@@ -138,17 +209,11 @@ class JsonFile(ContentFile):
             Attila Kovacs
         """
 
-        # Encrypt the file before saving
-        cipher = AESCipher(self._cb_retrieve_key())
-        encrypted_content = cipher.encrypt(json.dumps(self._content))
+        del compact
 
-        try:
-            with open(self._path, 'wb') as encrypted_file:
-                encrypted_file.write(encrypted_content)
-        except OSError as exception:
-            raise RuntimeError(
-                'Failed to save the content of JSON file to {}.'.format(
-                    self._path)) from exception
+        raise NotImplementedError(
+            f'ContentFile.save_encrypted() has to be implemented by '
+            f'{self.__class__.__name__}')
 
     def load_encrypted(self) -> None:
 
@@ -164,24 +229,6 @@ class JsonFile(ContentFile):
             Attila Kovacs
         """
 
-        raw_content = None
-        try:
-            with open(self._path, 'rb') as raw_file:
-                raw_content = raw_file.read()
-        except OSError as exception:
-            raise InvalidInputError(
-                'Failed to read the file from disk: {}.'.format(
-                    self._path)) from exception
-
-        if raw_content is not None:
-
-            # Try to load as a regular JSON file
-            try:
-                cipher = AESCipher(self._cb_retrieve_key())
-                self._content = json.loads(
-                    cipher.decrypt(raw_content))
-            except json.JSONDecodeError as exception:
-                raise InvalidInputError(
-                    'Failed to parse the content of JSON file {}. '
-                    'Either the decryption key was wrong or the file '
-                    'is not a valid JSON file.'.format(self._path)) from exception
+        raise NotImplementedError(
+            f'ContentFile.load_encrypted() has to be implemented by '
+            f'{self.__class__.__name__}')
