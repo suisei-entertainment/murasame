10d91fcb1950858ab439e06481fffa0787087f9f
Attila Kovacs <kovee@suiseientertainment.com>
2021-02-03 15:31:20 +0100

    MUR-26: Added basic debug decorators.

---
+++ murasame/debug/decorators.py
@@ -0,0 +1,68 @@
+## ============================================================================
+##             **** Murasame Application Development Framework ****
+##                Copyright (C) 2019-2021, Suisei Entertainment
+## ============================================================================
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+## ============================================================================
+
+"""
+Contains the implementation of debug decorators.
+"""
+
+# Platform Imports
+from typing import Any, Callable
+from time import perf_counter
+
+# Murasame Imports
+from murasame.logging import LogWriter
+
+def debug(function: Callable) -> Any:
+
+    """
+    Allows logging of a function call including the arguments it was called
+    with.
+
+    Authors:
+        Attila Kovacs
+    """
+
+    def wrapper(*args, **kwargs) -> Any:
+        logger = LogWriter(channel_name='murasame.debug', cache_entries=True)
+        logger.debug(f'Calling {function.__name__}')
+        logger.debug(f'    - Args: {args}')
+        logger.debug(f'    - Kwargs: {kwargs}')
+        result = function(*args, **kwargs)
+        logger.debug(f'   > Result: {result}')
+        return result
+    return wrapper
+
+def time(function: Callable) -> Any:
+
+    """
+    Allows logging of the execution time of functions.
+
+    Authors:
+        Attila Kovacs
+    """
+
+    def wrapper(*args, **kwargs) -> Any:
+        logger = LogWriter(channel_name='murasame.debug', cache_entries=True)
+        start_time = perf_counter()
+        result = function(*args, **kwargs)
+        end_time = perf_counter()
+        duration = (end_time - start_time) * 1000
+        logger.debug(f'    -- Execution time: {duration:0.4f} ms.')
+        return result
+    return wrapper
