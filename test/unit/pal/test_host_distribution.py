2737b1111943a79422eb2b9802da8ecea7053e88
Attila Kovacs <kovee@suiseientertainment.com>
2021-02-18 15:24:55 +0100

    MUR-30: Added unit test support for Ubuntu 20.04.

--- test/unit/pal/test_host_distribution.py
+++ test/unit/pal/test_host_distribution.py
@@ -52,9 +52,9 @@ class TestHostDistribution:
         assert sut.FullName.startswith('Ubuntu 18.04') \
                or sut.FullName.startswith('Ubuntu 19.04') \
                or sut.FullName.startswith('Ubuntu 20.04')
-        assert sut.MajorVersion in (18, 19)
+        assert sut.MajorVersion in (18, 19, 20)
         assert sut.MinorVersion == 4
         assert sut.BuildNumber == -1
-        assert sut.VersionString in ('18.04', '19.04')
-        assert sut.Codename.lower() in ('bionic', 'disco')
+        assert sut.VersionString in ('18.04', '19.04', '20.04')
+        assert sut.Codename.lower() in ('bionic', 'disco', 'focal')
         assert sut.Like == 'debian'
