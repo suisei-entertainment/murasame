de81c45879c369b787fc6a7b854b01c85f5bd663
Attila Kovacs <kovee@suiseientertainment.com>
2021-06-05 18:49:46 +0200

    Working on splitting unit tests.

--- test/unit/exceptions/test_exceptions.py
+++ test/unit/exceptions/test_exceptions.py
@@ -55,18 +55,28 @@ class TestExceptions:
     def test_exception(self):
 
         """
-        Contains tests for the base FrameworkError class.
+        Tests that FrameworkError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise FrameworkError('test')
         except FrameworkError as error:
             assert error.errorcode == ErrorCodes.NOT_SET
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_exception_without_caller_inspection(self):
+
+        """
+        Tests that FrameworkError can be raised without caller inspection
+        enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise FrameworkError('test', inspect_caller=False)
         except FrameworkError as error:
@@ -76,18 +86,28 @@ class TestExceptions:
     def test_access_violation_error(self):
 
         """
-        Contains tests for the AccessViolationError class.
+        Tests that AccessViolationError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise AccessViolationError('test')
         except AccessViolationError as error:
             assert error.errorcode == ErrorCodes.PERMISSION_ERROR
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_access_violation_error_without_caller_inspection(self):
+
+        """
+        Tests that AccessViolationError can be raised without caller inspection
+        enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise AccessViolationError('test', inspect_caller=False)
         except AccessViolationError as error:
@@ -97,18 +117,28 @@ class TestExceptions:
     def test_already_registered_error(self):
 
         """
-        Contains tests for the AlreadyRegisteredError class.
+        Tests that AlreadyRegisteredError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise AlreadyRegisteredError('test')
         except AlreadyRegisteredError as error:
             assert error.errorcode == ErrorCodes.ALREADY_REGISTERED
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_already_registered_error_without_caller_inspection(self):
+
+        """
+        Tests that AlreadyRegisteredError can be raised without caller
+        inspection enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise AlreadyRegisteredError('test', inspect_caller=False)
         except AlreadyRegisteredError as error:
@@ -118,39 +148,59 @@ class TestExceptions:
     def test_already_exists_error(self):
 
         """
-        Contains tests for the AlreadyExistsError class.
+        Tests that AlreadyExistsError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise AlreadyExistsError('test')
         except AlreadyExistsError as error:
             assert error.errorcode == ErrorCodes.ALREADY_EXISTS
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_already_exists_error_without_caller_inspection(self):
+
+        """
+        Tests that AlreadyExistsError can be raised without caller inspection
+        enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise AlreadyExistsError('test', inspect_caller=False)
         except AlreadyExistsError as error:
             assert error.errorcode == ErrorCodes.ALREADY_EXISTS
             assert error.errormessage == 'test'
 
-    def test_input_error(self):
+    def test_invalid_input_error(self):
 
         """
-        Contains tests for the InvalidInputError class.
+        Tests that InvalidInputError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise InvalidInputError('test')
         except InvalidInputError as error:
             assert error.errorcode == ErrorCodes.INPUT_ERROR
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_invalid_input_error_without_caller_inspection(self):
+
+        """
+        Tests that InvalidInputError can be raised without caller inspection
+        enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise InvalidInputError('test', inspect_caller=False)
         except InvalidInputError as error:
@@ -160,18 +210,28 @@ class TestExceptions:
     def test_not_registered_error(self):
 
         """
-        Contains tests for the NotRegisteredError class.
+        Tests that NotRegisteredError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise NotRegisteredError('test')
         except NotRegisteredError as error:
             assert error.errorcode == ErrorCodes.NOT_REGISTERED
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_not_registered_error_without_caller_inspection(self):
+
+        """
+        Tests that NotRegisteredError can be raised without caller inspection
+        enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise NotRegisteredError('test', inspect_caller=False)
         except NotRegisteredError as error:
@@ -181,18 +241,28 @@ class TestExceptions:
     def test_permission_error(self):
 
         """
-        Contains tests for the AccessViolation class.
+        Tests that AccessViolation can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise AccessViolationError('test')
         except AccessViolationError as error:
             assert error.errorcode == ErrorCodes.PERMISSION_ERROR
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_permission_error_without_caller_inspection(self):
+
+        """
+        Tests that AccessViolation can be raised without caller inspection
+        enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise AccessViolationError('test', inspect_caller=False)
         except AccessViolationError as error:
@@ -202,18 +272,28 @@ class TestExceptions:
     def test_installation_failed_error(self):
 
         """
-        Contains tests for the InstallationFailedError class.
+        Tests that InstallationFailedError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise InstallationFailedError('test')
         except InstallationFailedError as error:
             assert error.errorcode == ErrorCodes.INSTALL_FAILED
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_installation_failed_error_without_caller_inspection(self):
+
+        """
+        Tests that InstallationFailedError can be raised without caller
+        inspection enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise InstallationFailedError('test', inspect_caller=False)
         except InstallationFailedError as error:
@@ -223,18 +303,28 @@ class TestExceptions:
     def test_missing_requirement_error(self):
 
         """
-        Contains tests for the MissingRequirementError class.
+        Tests that MissingRequirementError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise MissingRequirementError('test')
         except MissingRequirementError as error:
             assert error.errorcode == ErrorCodes.MISSING_REQUIREMENT
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_missing_requirement_error_without_caller_inspection(self):
+
+        """
+        Tests that MissingRequirementError can be raised without caller
+        inspection enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise MissingRequirementError('test', inspect_caller=False)
         except MissingRequirementError as error:
@@ -244,18 +334,28 @@ class TestExceptions:
     def test_uncaught_exception_error(self):
 
         """
-        Contains tests for the UncaughtExceptionError class.
+        Tests that UncaughtExceptionError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise UncaughtExceptionError('test')
         except UncaughtExceptionError as error:
             assert error.errorcode == ErrorCodes.UNCAUGHT_EXCEPTION
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_uncaught_exception_error_without_caller_inspection(self):
+
+        """
+        Tests that UncaughtExceptionError can be raised without caller
+        inspection enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise UncaughtExceptionError('test', inspect_caller=False)
         except UncaughtExceptionError as error:
@@ -265,18 +365,28 @@ class TestExceptions:
     def test_invalid_license_key_error(self):
 
         """
-        Contains tests for the InvalidLicenseKeyError class.
+        Tests that InvalidLicenseKeyError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise InvalidLicenseKeyError('test')
         except InvalidLicenseKeyError as error:
             assert error.errorcode == ErrorCodes.LICENSE_ERROR
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_invalid_license_key_error_without_caller_inspection(self):
+
+        """
+        Tests that InvalidLicenseKeyError can be raised without caller
+        inspection enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise InvalidLicenseKeyError('test', inspect_caller=False)
         except InvalidLicenseKeyError as error:
@@ -286,18 +396,28 @@ class TestExceptions:
     def test_database_operation_error(self):
 
         """
-        Contains tests for the DatabaseOperationError class.
+        Tests that DatabaseOperationError can be raised.
+
+        Authors:
+            Attila Kovacs
         """
 
-        # STEP 1 - Test that the exception can be raised.
         try:
             raise DatabaseOperationError('test')
         except DatabaseOperationError as error:
             assert error.errorcode == ErrorCodes.DATABASE_ERROR
             assert error.errormessage == 'test'
 
-        # STEP 2 - Test that the exception can be raised without caller
-        # inspection.
+    def test_database_operation_error_without_caller_inspection(self):
+
+        """
+        Tests that DatabaseOperationError can be raised without caller
+        inspection enabled.
+
+        Authors:
+            Attila Kovacs
+        """
+
         try:
             raise DatabaseOperationError('test', inspect_caller=False)
         except DatabaseOperationError as error:
